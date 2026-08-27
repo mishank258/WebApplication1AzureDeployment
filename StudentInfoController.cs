3ba17fa08d120fa58b9bdf912ecd3812eef1d91a
Ishank <mishank258@gmail.com>
2026-08-28 03:39:42 +1000

    Add files via upload
       Add source code for Azure web app deployment task 2

---
+++ StudentInfoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentInfoController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public StudentInfoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.StudentName = _configuration["StudentSettings:Name"] ?? "Not Configured";
+            ViewBag.StudentId = _configuration["StudentSettings:StudentId"] ?? "Not Configured";
+            ViewBag.Unit = _configuration["StudentSettings:Unit"] ?? "Not Configured";
+            ViewBag.ServerTime = DateTime.Now.ToString("dd MMM yyyy, hh:mm:ss tt");
+            return View();
+        }
+    }
+}
